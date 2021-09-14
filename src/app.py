c9b8abc14a2eecd1c6893b9d0cb738f226590c3c
Shawntwj <shawntwj20@gmail.com>
2021-09-14 19:03:35 +0800

    AHHHHHHHH

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
-import flask, time
+import flask
+import time
 
 app = flask.Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Welcome!!! ",time.localtime
+    return "Welcome!!! ", time.localtime

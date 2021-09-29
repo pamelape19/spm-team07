3b3cc5b01076641c18c1fb67b36edcc7a64167d5
clara <saltedclaramel19@gmail.com>
2021-09-29 22:48:53 +0800

    added conditional rendering for course design btn

--- app/http/web/app/src/components/edit-course/EditCourse.js
+++ app/http/web/app/src/components/edit-course/EditCourse.js
@@ -6,6 +6,7 @@ import ModalComponent from '../general/ModalComponent';
 
 import './css/editCourse.css';
 import EnrolledClassSample from '../../resources/enrolledClassSample.png';
+import Trash from '../../resources/trash.png';
 
 class EditCourse extends Component{
     constructor(props){
@@ -13,18 +14,31 @@ class EditCourse extends Component{
         this.state = {
             numChapters: 2,
             listChapters: [1],
-            courseId: 111
+            courseId: 111,
+            courseDesignAdded: false,
+            hideTrashCourseDesign: true,
         }
         this.handleAdd = this.handleAdd.bind(this);
+        this.showEditCourseDesign = this.showEditCourseDesign.bind(this);
+        this.clearCourseDesign = this.clearCourseDesign.bind(this);
     }
     handleAdd = () => {
-
         this.setState({
-            
             numChapters: this.state.numChapters + 1,
             listChapters: [...this.state.listChapters, this.state.numChapters],
         });
-
+    }
+    showEditCourseDesign(){
+        this.setState({
+            courseDesignAdded: true,
+            hideTrashCourseDesign: false,
+        })
+    }
+    clearCourseDesign(){
+        this.setState({
+            courseDesignAdded: false,
+            hideTrashCourseDesign: true,
+        })
     }
  
     render(){
@@ -33,6 +47,18 @@ class EditCourse extends Component{
                                         <b>Note: </b>
                                         All files should be at least 720p and less than 4.0. GB.
                                     </div>
+        // conditional rendering for course design's button
+        let courseDesignBtn;
+        if ( this.state.courseDesignAdded === false ){
+            courseDesignBtn = <button className="course-design-btn" onClick={ this.showEditCourseDesign }>
+                            <ModalComponent btnName="Upload Content" body={ uploadCourseDesign }/>
+                        </button>
+        }
+        else{
+            courseDesignBtn = <button className="course-design-btn" onClick={ this.showEditCourseDesign }>
+                            <ModalComponent btnName="Edit Content" body={ uploadCourseDesign }/>
+                        </button>
+        }
         return(
             <div>
                 <Container className="edit-course-header">
@@ -55,9 +81,12 @@ class EditCourse extends Component{
                     <Card className="card-content-layout">
                         <div className="course-desc"> 
                             Class Design Document
+                            <span hidden={ this.state.hideTrashCourseDesign }>
+                                <button className="trash-btn" onClick={ this.clearCourseDesign }><img src={ Trash } alt=""/></button>
+                            </span>
                         </div>
                         <span>
-                            <ModalComponent btnName="Upload Content" body={ uploadCourseDesign }/>
+                            { courseDesignBtn }
                         </span>
                     </Card>
 

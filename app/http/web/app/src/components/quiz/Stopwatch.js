66abb2f4d0d379babb3bc0b3cf95390d2d94d73c
clara <saltedclaramel19@gmail.com>
2021-09-29 18:21:16 +0800

    updated stopwatch

--- app/http/web/app/src/components/quiz/Stopwatch.js
+++ app/http/web/app/src/components/quiz/Stopwatch.js
@@ -1,15 +1,29 @@
 import { React, Component } from 'react';
 
+import './css/test.css';
+
 class Stopwatch extends Component{
     constructor(props){
         super(props);
     }
     render(){
-        const { timeElapsed } = this.props;
+        const minutes = Math.floor( this.props.timeElapsed )
+        const seconds = Math.round(( this.props.timeElapsed - minutes )*60, 2)
         return(
             <div className="list-group">
                 <div className="list-group-item active">Time Left (In Minutes)</div>
-                <div className="list-group-item"><h1>{ timeElapsed }</h1></div>
+                <div className="list-group-item stopwatch-row">
+                    <div className="stopwatch-col">
+                        <div className="minutes">Min</div>
+                        <div></div>
+                        <div className="seconds">Sec</div>
+                    </div>
+                    <div className="stopwatch-col">
+                        <h4 className="minutes">{ minutes }</h4>
+                        <h4>:</h4>
+                        <h4 className="seconds">{ seconds }</h4>
+                    </div>
+                </div>
             </div>
         )
     }

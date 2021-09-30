e29df6e9c40947508145202943d3364a32730139
Shawntwj <shawntwj20@gmail.com>
2021-09-30 23:45:29 +0800

    Finished create course page

--- app/http/web/app/src/pages/hr/CreateCourse.js
+++ app/http/web/app/src/pages/hr/CreateCourse.js
@@ -6,31 +6,73 @@ import "./css/createCourse.css";
 class CreateCourse extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            completedCourse: ["a","b"],
-            currentlyLearning:[]
-        }
  
     }
     render(){
         return(
             <div style={{margin: '3% 0'}}>
                 {/* <img src={banner} alt="" style={{width: '100%'}}/> */}
-                <Container style={{marginTop: '5%'}}>
-                    
-                    <div className="content"> 
+                <Container style={{marginTop: '5%'}} class="contentthing">
 
 
-                        <div className="headerspace name"> 
-
-
-                            <span className="headertext"> Name: </span>  Mr dubibird 
-                            <input type="text" readonly class="form-control-plaintext" id="quizTitle" value={" "+"Chapter 1 - What is 3D Printing?"}/>
+                <div class="form-container">
+                    <form className="quiz-form form-course-create">
+                        <div className="quiz-row">
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label label-course-create">Course Name </label>
+                                <div class="col-sm-9">
+                                <input type="text" readonly class="form-control" id="courseTitle" placeholder={" "+"Chapter XX - Enter course name"}/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="quiz-row">
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label label-course-create"> Number of Sections</label>
+                                <div class="col-sm-3">
+                                <input type="number" class="form-control" id="courseSectionCount" placeholder="Enter num of setions"/>
+                                </div>
+                            </div>
                         </div>
 
-
-                            <Button variant="secondary">Assign Learner </Button>
+                        <div className="quiz-row">
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label label-course-create"> Capacity </label>
+                                <div class="col-sm-3">
+                                <input type="number" class="form-control" id="courseSectionCount" placeholder="Enter max capacity"/>
+                                </div>
+                            </div>
                         </div>
+
+                        <div className="quiz-row">
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label label-course-create">  Start Date </label>
+                                <div class="col-sm-3">
+                                <input type="date" class="form-control" id="courseSectionCount" placeholder="Enter start date"/>
+                                </div>
+                            </div>
+                        </div>
+
+                        
+                        <div className="quiz-row">
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label label-course-create">  End Date </label>
+                                <div class="col-sm-3">
+                                <input type="date" class="form-control" id="courseSectionCount" placeholder="Enter end date"/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="create-btn">
+                            <div></div>
+
+                        </div>
+
+                        <Button type="submit" variant="secondary"> Create Course </Button>
+
+                    </form>
+                </div>
+                 
+
                      
                 </Container>
             </div>

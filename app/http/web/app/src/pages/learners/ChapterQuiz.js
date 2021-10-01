d75877b1040f513f3296dfc97a1341a9aee6d249
clara <saltedclaramel19@gmail.com>
2021-10-01 19:57:42 +0800

    fixed chapter quiz layout

--- app/http/web/app/src/pages/learners/ChapterQuiz.js
+++ app/http/web/app/src/pages/learners/ChapterQuiz.js
@@ -11,21 +11,20 @@ class ChapterQuiz extends Component{
     render(){
         return(
             <div>
-                <div className="sticky-top">
-                <Container className="chapter-quiz-header">
-                    <h1> Quiz 1 - Introduction to 3D Printing </h1>
-                    <div></div>
-                    <p> Estimated Length: 30 mins</p>
-                </Container>
-                <hr/>
+                <div className="chapter-quiz-sticky-top">
+                    <Container className="chapter-quiz-header">
+                        <h1> Quiz 1 - Introduction to 3D Printing </h1>
+                        <p> Estimated Length: 30 mins</p>
+                    </Container>
+                    <hr/>
                 </div>
-                <Container className = "questions">
+                <Container className = "chapter-quiz-questions">
                     <McqQn qn_no = { 1 } qn = "What is 3D Printing?" options = {["lorem ipsum", "ipsum lorem", "lorem ipsum", "ipsum lorem"]} />
                     <McqQn qn_no = { 2 } qn = "3D Printing can print 3D" options = {["True", "False"]}/>
-                <div className = "buttons">
+                <div className = "chapter-quiz-buttons">
                     <div></div>
-                <Button type="submit" variant="secondary" >Save</Button>{' '}
-                <Button type="submit">Submit</Button>{' '}
+                    <Button type="submit" variant="secondary" >Save</Button>{' '}
+                    <Button type="submit">Submit</Button>{' '}
                 </div>   
                 
                 </Container>

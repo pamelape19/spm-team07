53edd9265c5bb29e28119b3102ded96df0f7f46b
clara <saltedclaramel19@gmail.com>
2021-10-05 19:16:36 +0800

    made learnersHome dynamic -get course name from db

--- app/http/web/app/src/components/cards/CourseCard.js
+++ app/http/web/app/src/components/cards/CourseCard.js
@@ -7,14 +7,14 @@ class CourseCard extends Component{
         super(props);
     }
     render(){
-        const link = this.props.link;
+        const { link, name }= this.props;
         return(
             <div>
                 <a style={{ cursor: 'pointer'}} href={ link }>
                     <Card style={{ width: '16rem', boxShadow: '0 0 10px 0 rgba(100, 100, 100, 0.26)' }}>
                         <Card.Img variant="top" src={ bg } />
                         <div style={{ padding: 20 }}>
-                            <Card.Title style={{textAlign: 'left'}}>Basics of 3D Printing</Card.Title>
+                            <Card.Title style={{textAlign: 'left'}}>{name}</Card.Title>
                             <Card.Text>Classes offered - 2</Card.Text>
                         </div>
                     </Card>

49c81efa7cd3838d57323303667378c2ec4f8259
Pam <89832474+pamelape19@users.noreply.github.com>
2021-10-26 03:11:25 +0800

    Update test_class_unit_test.py

--- app/http/api/test_class_unit_test.py
+++ app/http/api/test_class_unit_test.py
@@ -8,11 +8,10 @@ import json
 # set our application to testing mode
 app.testing = True
 # class: port 5003
-# classNo, capacity, startDateTime, endDateTime
 
 class TestClass(unittest.TestCase):
     def setUp(self):
-        self.ClassInput = CLASSES (1,'2021-10-04','2021-11-04', 40, 'Introduction to IBM WorkCentre', 'alexlim@allinone.com')
+        self.ClassInput = CLASSES (5, '2021-10-08', '2021-11-08', 40, 'SOP for Repair Work', 'boblee@allinone.com')
         self.app = app.test_client()
 
     def teardown(self):
@@ -20,17 +19,17 @@ class TestClass(unittest.TestCase):
         
     def test_JSON(self):
         JsonInput = self.ClassInput.json()
-        JsonCheck = {'CNo': 1, 'Start_date': '2021-10-04', 'End_date': '2021-11-04', 'Capacity': 40, 'Course_name': 'Introduction to IBM WorkCentre', 'Trainer': 'alexlim@allinone.com'}
+        JsonCheck = {'CNo': 5, 'Start_date': '2021-10-08', 'End_date': '2021-11-08', 'Capacity': 40, 'Course_name': 'SOP for Repair Work', 'Trainer': 'boblee@allinone.com'}
         self.assertEqual(JsonCheck, JsonInput)
 
     def test_get_all_classes(self):
         Response = self.app.get("/")
         Data = json.loads(Response.get_data())['data']['classes']
-        FirstData = Data[0]["CNo"]
-        # LastData = Data[4]["Course_name"]
+        FirstData = Data[0]["Course_name"]
+        LastData = Data[-1]["Course_name"]
         # self.assertEqual(Response.status_code, 200)
-        self.assertEqual("1", FirstData)
-        # self.assertEqual("Introduction to IBM WorkCentre", LastData)
+        self.assertEqual("Introduction to Canon WorkCentre", FirstData)
+        self.assertEqual("SOP for Repair Work", LastData)
 
 if __name__ == '__main__':
     unittest.main()

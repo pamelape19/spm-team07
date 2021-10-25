6e6d9520f9b9c333a29ebdfba62f87c0a84082ea
Pam <89832474+pamelape19@users.noreply.github.com>
2021-10-25 19:18:04 +0800

    Update class_unittest.py

--- app/http/api/class_unittest.py
+++ app/http/api/class_unittest.py
@@ -8,5 +8,8 @@ class TestClass(unittest.TestCase):
     def test_getAllClasses(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
+    def test_getSpecificClass(self):
+        self.assertEqual('Introduction to IBM WorkCentre', '1')
+
 if __name__ == '__main__':
     unittest.main()

59365024c370ac2ba5b18a5cc2a8df0c65eb48c1
clara <saltedclaramel19@gmail.com>
2021-10-25 17:02:09 +0800

    skeleton set up for class unittest

--- app/http/api/class_unittest.py
+++ app/http/api/class_unittest.py
@@ -1,12 +1,12 @@
 import unittest
-from classes import get_all_classes
+from classes import CLASSES
 
 # class: port 5003
 # classNo, capacity, startDateTime, endDateTime
 
 class TestClass(unittest.TestCase):
-    def getAllClasses(self):
-        self.retrieve = get_all_classes()
+    def test_getAllClasses(self):
+        self.assertEqual('foo'.upper(), 'FOO')
 
 if __name__ == '__main__':
     unittest.main()

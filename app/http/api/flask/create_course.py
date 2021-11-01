d24d67a514ba6cb63f528082bb8455f5f74441b3
Shawntwj <shawntwj20@gmail.com>
2021-11-01 17:19:35 +0800

    changed ports

--- app/http/api/flask/create_course.py
+++ app/http/api/flask/create_course.py
@@ -17,7 +17,7 @@ from os import environ
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
-    'dbURL') or 'mysql+mysqlconnector://root@localhost:3308/lms'   
+    'dbURL') or 'mysql+mysqlconnector://root@localhost:3306/lms'   
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
                                            'pool_recycle': 280}

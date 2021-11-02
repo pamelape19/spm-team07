51d81a3dd8e9425628f0adf218a9e585afb8b22c
Shawntwj <shawntwj20@gmail.com>
2021-11-02 17:05:58 +0800

    fixed bug with chapter post

--- app/http/api/flask/chapter.py
+++ app/http/api/flask/chapter.py
@@ -55,8 +55,10 @@ def get_all_chapter():
 
 @app.route("/<string:course_name>/<int:CNo>/<int:chapterNo>/<string:chapter_name>", methods=['POST'])
 def update_chapter(course_name, CNo, chapterNo, chapter_name):
+
     chapter = CHAPTER.query.filter_by(course_name=course_name, CNo=CNo).first()
-    if (chapter):
+
+    if (chapter != None):
         new_chapter = CHAPTER(chapterNo=chapterNo, chapter_name=chapter_name, CNo=CNo, course_name=course_name)
         try:
             db.session.add(new_chapter)
@@ -75,7 +77,6 @@ def update_chapter(course_name, CNo, chapterNo, chapter_name):
             print(e)
             return 'Chapter name could not be updated'
         return 'Chapter name has been updated'
-        
 
     
 

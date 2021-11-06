f94451d94dd6ee2e1fb047e66f089e68651562c8
SHAAMINI G <shaaminig.2019@business.smu.edu.sg>
2021-11-06 20:32:41 +0800

    Update chapter.py

--- app/http/api/flask/chapter.py
+++ app/http/api/flask/chapter.py
@@ -7,7 +7,7 @@ from os import environ
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
-    'dbURL')or 'mysql+mysqlconnector://root@localhost:3306/lms'
+    'dbURL') or 'mysql+mysqlconnector://root@localhost:3306/lms'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
                                            'pool_recycle': 280}
@@ -25,7 +25,7 @@ class CHAPTER (db.Model):
     CNo = db.Column(db.Integer, nullable=False, primary_key=True)
     course_name = db.Column(db.String(100), nullable=False, primary_key=True)
 
-    def __init__(self, chapterNo, chapter_name, CNo,course_name):
+    def __init__(self, chapterNo, chapter_name, CNo, course_name):
         self.chapterNo = chapterNo
         self.CNo = CNo
         self.chapter_name = chapter_name
@@ -78,10 +78,13 @@ def get_class_chapters(course_name, CNo):
     ), 404
 
 
-@app.route("/<string:course_name>/<int:CNo>/<int:chapterNo>/<string:chapter_name>", methods=['POST'])
+@app.route("/<string:course_name>/<int:CNo>/",
+           "<int:chapterNo>/<string:chapter_name>", methods=['POST'])
 def update_chapter(course_name, CNo, chapterNo, chapter_name):
     print(course_name, CNo, chapterNo, chapter_name)
-    new_chapter = CHAPTER(chapterNo=chapterNo, chapter_name=chapter_name, CNo=CNo, course_name=course_name)
+    new_chapter = CHAPTER(chapterNo=chapterNo, 
+                          chapter_name=chapter_name, CNo=CNo, 
+                          course_name=course_name)
     try:
         db.session.add(new_chapter)
         print('add')
@@ -91,6 +94,7 @@ def update_chapter(course_name, CNo, chapterNo, chapter_name):
         print(e)
         return 'Chapter could not be added'
     return 'Chapter has been added'
+
 
 if __name__ == '__main__':
 

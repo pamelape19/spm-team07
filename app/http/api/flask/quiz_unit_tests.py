5567706d5419c3cb0cbc0ac7dc95b2d6d7343fad
SHAAMINI G <shaaminig.2019@business.smu.edu.sg>
2021-11-06 15:52:43 +0800

    fix for code analysis

--- app/http/api/flask/quiz_unit_tests.py
+++ app/http/api/flask/quiz_unit_tests.py
@@ -3,15 +3,16 @@ import unittest
 from sqlalchemy.sql.expression import null
 from quiz import QUIZ
 from quiz import app
-import json 
-import unittest
+import json
 # set our application to testing mode
 # Shawn
 app.testing = True
 
 class TestQuiz(unittest.TestCase):
+
     def setUp(self):
-        self.QuizInput = QUIZ(1001, 1, "Introduction to IBM WorkCentre", "Introduction", null, 1)
+        self.QuizInput = QUIZ(1001, 1, "Introduction to IBM WorkCentre", 
+        "Introduction", null, 1)
         self.app = app.test_client()
 
     def teardown(self):
@@ -19,7 +20,8 @@ class TestQuiz(unittest.TestCase):
 
     def test_JSON(self):
         JsonInput = self.QuizInput.json()
-        JsonCheck = {'quizID': 1001, 'CNo': 1, 'course_name': 'Introduction to IBM WorkCentre', 'chapter_name': 'Introduction', 'duration': null, 'total_questions': 1}
+        JsonCheck = {'quizID': 1001, 'CNo': 1, 'course_name': 'Introduction to IBM WorkCentre', 
+        'chapter_name': 'Introduction', 'duration': null, 'total_questions': 1}
         self.assertEqual(JsonCheck,JsonInput)
 
     def test_get_all_quiz(self):
@@ -30,7 +32,7 @@ class TestQuiz(unittest.TestCase):
         NumberofQuiz = len(Data)
         self.assertEqual(24, NumberofQuiz)
         self.assertEqual("1001", FirstQuizID)
-        self.assertEqual("3201", LastQuizID)      
+        self.assertEqual("3201", LastQuizID)
 
     def test_get_specific_quiz(self):
         Response = self.app.get("/Introduction to IBM WorkCentre/1")
@@ -40,9 +42,9 @@ class TestQuiz(unittest.TestCase):
         CourseName = Data[0]["course_name"]
         SpecificQuizID =  Data[0]["quizID"]
         self.assertEqual(1, CNo)
-        self.assertEqual("Introduction to IBM WorkCentre",CourseName)
+        self.assertEqual("Introduction to IBM WorkCentre", CourseName)
         self.assertEqual("1001", SpecificQuizID)
-    ## def test_get_certain_quiz(self):
+    # def test_get_certain_quiz(self):
 
 
 if __name__ == "__main__":

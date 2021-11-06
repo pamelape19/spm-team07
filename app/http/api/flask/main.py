17858f77e69f55d54870d81de3a278d57e8a0fbc
SHAAMINI G <shaaminig.2019@business.smu.edu.sg>
2021-11-06 14:27:53 +0800

    edit blank lines

--- app/http/api/flask/main.py
+++ app/http/api/flask/main.py
@@ -22,7 +22,7 @@ import train #noqa: E402,F401
 @app.route('/')
 def hello_world():
     return "<p>Hello world, It's me</p>"
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-

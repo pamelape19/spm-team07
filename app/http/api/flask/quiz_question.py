4c8326ed0f2be262c78cc54a16d53a4408e93c62
SHAAMINI G <shaaminig.2019@business.smu.edu.sg>
2021-11-09 03:00:56 +0800

    Update quiz_question.py

--- app/http/api/flask/quiz_question.py
+++ app/http/api/flask/quiz_question.py
@@ -21,11 +21,11 @@ CORS(app)
 class QUIZ_QUESTION (db.Model):
     __tablename__ = 'QUIZ_QUESTION'
     question = db.Column(db.String(1000), nullable=False)
-    questionNo = db.Column(db.Integer, nullable=False, primary_key=True )
-    question_type = db.Column(db.Enum('mcq', 't/f'), nullable=False ) 
-    quizID= db.Column(db.String(50),nullable=False, primary_key=True)
+    questionNo = db.Column(db.Integer, nullable=False, primary_key=True)
+    question_type = db.Column(db.Enum('mcq', 't/f'), nullable=False)
+    quizID = db.Column(db.String(50), nullable=False, primary_key=True)
 
-    def __init__(self, question, questionNo, question_type,quizID):
+    def __init__(self, question, questionNo, question_type, quizID):
         self.question = question
         self.questionNo = questionNo
         self.question_type = question_type
@@ -33,7 +33,7 @@ class QUIZ_QUESTION (db.Model):
 
     def json(self):
         return {"question": self.question, "questionNo": self.questionNo,
-                "question_type": self.question_type,"quizID": self.quizID}
+                "question_type": self.question_type, "quizID": self.quizID}
 
 
 @app.route("/")
@@ -44,7 +44,7 @@ def get_all_quiz_question():
             {
                 "code": 200,
                 "data": {
-                    "quiz_question": [quiz_question.json() for quiz_question in quiz_question_list]
+                    "quiz_question": [quiz_question.json() for quiz_question in quiz_question_list]  # noqa: E501
                 }
             }
         )
@@ -83,8 +83,10 @@ def addNewQuiz(quizID):
     try:
         for qus in data["data"]:
             print(qus)
-            new_quiz = QUIZ_QUESTION(question=qus["question"], questionNo=qus["question_no"],
-                                     question_type=qus['question_type'], quizID=quizID )
+            new_quiz = QUIZ_QUESTION(question=qus["question"],
+                                     questionNo=qus["question_no"],
+                                     question_type=qus['question_type'],
+                                     quizID=quizID)
             print(new_quiz)
             try:
                 db.session.add(new_quiz)
@@ -98,6 +100,6 @@ def addNewQuiz(quizID):
         return 'Quiz could not be added'
     return 'Quiz has been added'
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5009, debug=True)
-

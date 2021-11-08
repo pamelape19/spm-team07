707f74c8d3d2deeba27da87c0d58b77dd7d83b95
SHAAMINI G <shaaminig.2019@business.smu.edu.sg>
2021-11-09 02:56:50 +0800

    Update quiz.py

--- app/http/api/flask/quiz.py
+++ app/http/api/flask/quiz.py
@@ -8,7 +8,7 @@ from os import environ
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
-    'dbURL')or 'mysql+mysqlconnector://root@localhost:3306/lms'
+    'dbURL') or 'mysql+mysqlconnector://root@localhost:3306/lms'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
                                            'pool_recycle': 280}
@@ -20,13 +20,12 @@ CORS(app)
 
 class QUIZ (db.Model):
     __tablename__ = 'QUIZ'
-    quizID  = db.Column(db.String(50), nullable=False, primary_key=True)
+    quizID = db.Column(db.String(50), nullable=False, primary_key=True)
     CNo = db.Column(db.Integer, nullable=False)
     course_name = db.Column(db.String(100), nullable=False)
-    chapter_name  = db.Column(db.String(100))
+    chapter_name = db.Column(db.String(100))
     duration = db.Column(db.Integer)
     total_questions = db.Column(db.Integer, nullable=False)
-
 
     def __init__(self, quizID, CNo, course_name, chapter_name, duration, total_questions):
         self.quizID = quizID
@@ -37,7 +36,8 @@ class QUIZ (db.Model):
         self.total_questions = total_questions
 
     def json(self):
-        return {"quizID": self.quizID, "CNo": self.CNo, "course_name":self.course_name,
+        return {"quizID": self.quizID, "CNo": self.CNo,
+                "course_name": self.course_name,
                 "chapter_name": self.chapter_name, "duration": self.duration,
                 "total_questions": self.total_questions}
 
@@ -64,13 +64,14 @@ def get_all_quiz():
 
 @app.route("/<string:course_name>/<int:CNo>")
 def find_quizzes_by_course(course_name, CNo):
-    course_quizzes = QUIZ.query.filter_by(course_name=course_name, CNo=CNo).all()
+    course_quizzes = QUIZ.query.filter_by(course_name=course_name,
+                                          CNo=CNo).all()
     if course_quizzes:
         return jsonify(
             {
                 "code": 200,
                 "data": {
-                    "courseQuizzes": [course_quiz.json() for course_quiz in course_quizzes]
+                    "courseQuizzes": [course_quiz.json() for course_quiz in course_quizzes]  # noqa: E501
                 }
             }
         )
@@ -85,7 +86,8 @@ def find_quizzes_by_course(course_name, CNo):
 @app.route("/<string:course_name>/<int:CNo>/<string:chapter_name>")
 def find_quizzes_by_chapter(course_name, CNo, chapter_name):
     chapter_quiz = QUIZ.query.filter_by(course_name=course_name,
-                                        CNo=CNo, chapter_name=chapter_name).first()
+                                        CNo=CNo,
+                                        chapter_name=chapter_name).first()
     if chapter_quiz:
         return jsonify(
             {
@@ -106,7 +108,7 @@ def addNewQuiz(course_name, CNo, chapter_name, quizID, quizNum, duration):
 
     print(course_name, CNo, chapter_name, quizID, quizNum, duration)
     new_quiz = QUIZ(course_name=course_name, CNo=CNo, chapter_name=chapter_name,
-                    quizID=quizID, total_questions=quizNum, duration=duration )
+                    quizID=quizID, total_questions=quizNum, duration=duration)
     try:
         db.session.add(new_quiz)
         print('add')
